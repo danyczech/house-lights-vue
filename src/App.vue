fb1e16bdeec7f1836fd6eb33d32570add8fc2fa5
danakal <dana.kalcikova@centrum.cz>
2023-03-12 09:11:18 +0100

    adding API from Strapi

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <header>
     <h1>Chytrý dům</h1>
   </header>
-
-  <main>
+  <p v-if="isLoading">Is loading</p>
+  <p v-else-if="!isLoading&&error"> {{ error }}</p>
+  <main v-else>
     <OneDetail :room="rooms[roomDetail]" :id="roomDetail" @switch-light="switchLight" />
     <AllLights :rooms="rooms" @change-room="setDetail" />
   </main>
@@ -17,38 +18,11 @@ export default {
   components: { AllLights, OneDetail }, 
   data () {
     return {
-      rooms: [
-        {
-          name: "Obývací pokoj",
-          lightOn: false,
-        },
-        {
-          name: "Ložnice",
-          lightOn: true,
-        },
-        {
-          name: "Dětský pokoj",
-          lightOn: false,
-        },
-        {
-          name: "Koupelna",
-          lightOn: false,
-        },
-        {
-          name: "Kuchyně",
-          lightOn: true,
-        },
-        {
-          name: "Chodba",
-          lightOn: false,
-        },
-        {
-          name: "Záchůdek",
-          lightOn: false,
-        },
-      ],
-
+      sourceData: [],
+      rooms: [],
       roomDetail: 0, //starting room in detail
+      isLoading: true,
+      error: null
     };
   },
 
@@ -58,8 +32,28 @@ export default {
     },
     switchLight(index) {
       this.rooms[index].lightOn = !this.rooms[index].lightOn;
+    },
+    loadRooms(){
+      this.isLoading = true;
+      fetch("http://localhost:1337/api/rooms")
+        .then((res) => res.json())
+        .then((data) => {
+          this.isLoading = false;
+          this.sourceData = data.data;
+          this.sourceData.forEach(item => {
+            const newItem = {'name': item.attributes.name, 'lightOn': item.attributes.lightOn, 'id': item.id};
+            this.rooms.push(newItem);
+          })
+        })
+        .catch(err => {
+          this.isLoading = false;
+          this.error = err;
+        });
     }
   },
+  mounted() {
+    this.loadRooms();
+  }  
 };
 </script>
 
